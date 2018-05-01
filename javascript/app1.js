bcbdf42cb76d4c86c7e7685f5dfd35da1251e495
William Tronvig <wdtronvig289840@gmail.com>
2018-05-01 15:36:15 -0700

    tried rearranging some items, still figuring out design

--- javascript/app1.js
+++ javascript/app1.js
@@ -1,6 +1,15 @@
 'use strict';
 
+var projectCube1;
+var projectCube2;
+var projectCube3;
+var projectCube4;
+var projectCube5;
+
+var projectsArray = [];
+
 var PortfolioPageOne = function (projects, aboutme) {
     this.projects = projects;
     this.aboutme = aboutme;
 }
+
