47d2be9c947f4b2999165073e0167f5a085c41e3
William Tronvig <wdtronvig289840@gmail.com>
2018-05-02 00:29:06 -0700

    playing around with some code online

--- javascript/app1.js
+++ javascript/app1.js
@@ -1,15 +1,23 @@
-'use strict';
+// 'use strict';
 
-var projectCube1;
-var projectCube2;
-var projectCube3;
-var projectCube4;
-var projectCube5;
+// var projectCube1;
+// var projectCube2;
+// var projectCube3;
+// var projectCube4;
+// var projectCube5;
 
-var projectsArray = [];
+// var projectsArray = [];
 
-var PortfolioPageOne = function (projects, aboutme) {
-    this.projects = projects;
-    this.aboutme = aboutme;
-}
+// var PortfolioPageOne = function (projects, aboutme) {
+//     this.projects = projects;
+//     this.aboutme = aboutme;
+// }
 
+// 'use strict';
+
+// var PortfolioPageTwo = function (name, image, links, menu) {
+//     this.name = name;
+//     this.image = image;
+//     this.links = links;
+//     this.menu = menu;
+// }
