4137451efcdd640f6955042490fb943f9b8a2d9d
William Tronvig <wdtronvig289840@gmail.com>
2018-05-30 12:14:02 -0700

    finished lab 3

--- javascript/events.js
+++ javascript/events.js
@@ -5,3 +5,15 @@ $('#menu-icon').on('click', function() {
     $(this).toggleClass('icon-menu icon-happy2')
     })
 });
+
+
+$(".icon-home").on('click', function() {
+    $('#aboutme').hide();
+    $('#home').show();
+});
+
+
+$(".icon-address-book").on('click', function() {
+    $('#home').hide();
+    $('#aboutme').show();
+});
