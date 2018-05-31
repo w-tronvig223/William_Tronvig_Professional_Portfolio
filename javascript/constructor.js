6869c13c0c8de0bade1aa0e37ac4e15e47905f13
William Tronvig <wdtronvig289840@gmail.com>
2018-05-31 16:03:19 -0700

    working through lab 6

--- javascript/constructor.js
+++ javascript/constructor.js
@@ -1,27 +1,30 @@
 'use strict';
 
-var projects = [];
+// var projects = [];
 
 function Portfolio(rawDataObj) {
-    this.cubeObjectId1 = rawDataObj.cubeObjectId1,
-    this.title = rawDataObj.title,
-    this.author = rawDataObj.author,
-    this.websiteUrl = rawDataObj.websiteUrl,
-    this.wentLiveOn = rawDataObj.wentLiveOn,
-    this.projectDescription = rawDataObj.projectDescription
-};
+    this.cubeObjectId1 = rawDataObj.cubeObjectId1;
+    this.title = rawDataObj.title;
+    this.author = rawDataObj.author;
+    this.websiteUrl = rawDataObj.websiteUrl;
+    this.wentLiveOn = rawDataObj.wentLiveOn;
+    this.projectDescription = rawDataObj.projectDescription;
+}
+
+Portfolio.all [];
 
 Portfolio.prototype.toHtml = function() {
-
-    var template = $('#projects').html();
-    var compile = Handlebars.compile(template);
+    let template = Handlebars.compile($('#projects').text());
+    
+    // var template = $('#projects').html(); took out 19 and 20 first
+    // var compile = Handlebars.compile(template);
 
     // var $newProject = $('.template').clone();
     // $newProject.removeClass('template');    
 
     // if (!this.wentLiveOn) $newProject.addClass('draft');
     // $newProject.data('category', this.category);
-    $('main').append(compile(this));
+    // $('main').append(compile(this)); took out 27 next
     // $newProject.find('h1').text(this.title);
     // return $newProject;
 };
